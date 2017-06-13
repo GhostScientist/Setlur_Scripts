c2ad08329c45002dace920f6b9ccc50df657fffb
ghost <S0281634@salemstate.edu>
2017-06-12 22:03:04 -0400

    No longer spearman analysis. Now a simple data-building script

--- spearman.py
+++ spearman.py
@@ -1,52 +1,80 @@
-import xlrd, sys, scipy
+import xlrd
 import matplotlib.pyplot as plt
 import numpy as np
 
-excel_file = sys.argv[0] # Takes in path to excel file as argument.
+print("Now opening the workbook")
+supp_table = xlrd.open_workbook('/Users/dakotakim/Desktop/Setlur_Datasets/supplemental_table10.xlsx')
+print("Now opening the superseries")
+beta_values = xlrd.open_workbook('/Users/dakotakim/Desktop/Setlur_Datasets/GSE65820_series_matrix.xlsx')
+print("DONE!")
 
-"""Let me see if its simpler to create an external text_file with all
- of the relevant datasets and their respective paths. If I do that,
-it should make it much simpler to run the new scripts a single time.
-I will experiment with a respective file for output, perhaps as an image?
-This will require a method to create and associate Subject ID with their
-analysis/output. Also, multi-line comments are easier this way."""
+workspace_supp = supp_table.sheet_by_index(0)
+workspace_beta = beta_values.sheet_by_index(0)
 
-data = xlrd.open_workbook('/Users/dakotakim/Desktop/test_file.xlsx')
+# Making 4 lists, one for each category.
+category_1 = []
+category_2 = []
+category_3 = []
 
-# Create a list for both the chromosomes and the beta values
-# I'll start with 100 different ID's. I can see what kind of stuff
-# we'll be getting from this approach.
-# 119 beta values per sample
+category_col = 35
+sample_id_col = 2
+sample_title_row = 111
+begin_mine = 148
+end_mine = 100 # Only taking 10000 values for each subject.
+beta_val_col = 2
 
-workspace = data.sheet_by_index(0)
+used_subject_ids = []
 
-dict = {}
-beta_values = []
+# This will build expression data.
 
-for x in range(148, 248):
-    for i in range(2, 119):
+print("hello, let's see if this is reached.")
 
-        #print(str(workspace.cell(x,0).value) + ":" + str(workspace.cell(x,i).value))
-        if workspace.cell(x,i).value != '' :
-            beta_values.append(float(workspace.cell(x,i).value))
-    dict[workspace.cell(x,0).value] = (beta_values) # Maps each list to a CpG site.
-    beta_values = [] # Resets the list of beta values.
+for x in range(3, 50): # Will go through table 10.1's category list.
+    print("You've finally begun a loop!")
+    if (workspace_supp.cell(x, category_col).value) == '1':
+        sample_id_name = workspace_supp.cell(x, sample_id_col).value
+        if sample_id_name not in used_subject_ids:
+            used_subject_ids.append(sample_id_name)
+            for y in range(2, 115):
+                temp_name = workspace_beta.cell(sample_title_row,y).value
+                sliced_name = temp_name[9:]
+                if sample_id_name == sliced_name:
+                    for x in range(begin_mine, end_mine):
+                        print(float(workspace_beta.cell(beta_val_col, x).value))
+                        category_1.append(float(workspace_beta.cell(beta_val_col, x).value))
+        else:
+            print("this is breaking")
+            break
+    elif (workspace_supp.cell(x, category_col).value) == '2':
+        sample_id_name = workspace_supp.cell(x, sample_id_col).value
+        if sample_id_name not in used_subject_ids:
+            used_subject_ids.append(sample_id_name)
+            for y in range(2, 115):
+                temp_name = workspace_beta.cell(sample_title_row,y).value
+                sliced_name = temp_name[9:]
+                if sample_id_name == sliced_name:
+                    for x in range(begin_mine, end_mine):
+                        float(workspace_beta.cell(beta_val_col, x).value)
+                        category_2.append(float(workspace_beta.cell(beta_val_col, x).value))
+        else:
+            print("this is breaking")
+            break
+    elif (workspace_supp.cell(x, category_col).value) == '3':
+        sample_id_name = workspace_supp.cell(x, sample_id_col).value
+        if sample_id_name not in used_subject_ids:
+            used_subject_ids.append(sample_id_name)
+            for y in range(2, 115):
+                temp_name = workspace_beta.cell(sample_title_row,y).value
+                sliced_name = temp_name[9:]
+                if sample_id_name == sliced_name:
+                    for x in range(begin_mine, end_mine):
+                        float(workspace_beta.cell(beta_val_col, x).value)
+                        category_3.append(float(workspace_beta.cell(beta_val_col, x).value))
+        else:
+            print("this is breaking")
+            break
 
-box_list=[]
-my_chroms = []
+# Data has been acquired. Time to analyze
 
-for entry in dict.keys(): # For loop to plot Box plots for each CpG site.
-    my_chroms.append(str(entry))
-    box_list.append(dict[entry])
-
-
-
-temp = []
-for i in range(0, len(my_chroms)):
-    temp.append(i)
-
-x = np.array(temp)
-
-plt.boxplot(box_list,vert=1)
-plt.xticks(x, my_chroms, rotation=90, fontsize=10)
+plt.boxplot(category_1)
 plt.show()
